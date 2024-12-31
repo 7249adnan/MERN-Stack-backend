556548f9e38ea2efc717f36f009730e36cc4a4c4
Adnan Shaikh <53960360+7249adnan@users.noreply.github.com>
2024-12-31 15:25:38 +0530

    Update db.js conn string

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: "mysql.railway.internal",
-    user: "root",
-    password: "lKmvNmdfzlJABHOvwtWdGKVSvDRTiowJ",
-    database: "railway",
-    port: "80",
+    host: "nodedatabase.cxgc04oysmwb.ap-northeast-3.rds.amazonaws.com",
+    user: "admin",
+    password: "zelto#1234",
+    database: "lms",
+    port: "3306",
 });
 
 module.exports = pool.promise();
